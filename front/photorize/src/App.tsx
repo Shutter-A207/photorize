98eda2d077f7d2f1de87f13b4c0bd9d281e65ca5
chanhyun22 <bch0707@dongguk.edu>
2024-10-28 14:00:25 +0900

    feat: FE 프로젝트 초기 셋팅

---
+++ front/photorize/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "primereact/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hi123</h1>
+      <div className="card flex justify-content-center">
+        <Button label="Submit" />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ front/photorize/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  // important: true,
+};
