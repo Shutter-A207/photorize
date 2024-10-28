94b398dac2a43cf53b1b9757317c050a622f6b80
양지웅 <yju1028@naver.com>
2024-10-28 17:33:22 +0900

    Feat: 헤더 네비게이션 구현
    1. 뒤로 가기 기능 구현
    2. 페이지에 맞는 헤더  표시 기능 구현
    
    S11P31A207-220

--- front/photorize/src/App.tsx
+++ front/photorize/src/App.tsx
@@ -3,14 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Loading from "./pages/Loading";
 import Alarm from "./pages/Alarm/Alarm";
+<<<<<<< HEAD
 import Album from "./pages/Album/Album";
 import Record from "./pages/Album/Record";
 import Pose from "./pages/Pose/Pose";
 import Spot from "./pages/Spot/Spot";
+=======
+import Header from "./components/Common/Header";
+>>>>>>> 1c88519 (Feat: 헤더 네비게이션 구현)
 
 function App() {
   return (
     <>
+<<<<<<< HEAD
       <Routes>
         <Route path="/" element={<Loading />} />
         <Route path="/home" element={<Home />} />
@@ -19,6 +24,13 @@ function App() {
         <Route path="/album" element={<Album />} />
         <Route path="/pose" element={<Pose />} />
         <Route path="/spot" element={<Spot />} />
+=======
+      <Header title="앨범" />
+      <Routes>
+        <Route path="/" element={<Loading />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/notification" element={<Alarm />} />
+>>>>>>> 1c88519 (Feat: 헤더 네비게이션 구현)
       </Routes>
     </>
   );
