e93e510c4c5f92b3c391f4e5379ee58ad44c66e1
양지웅 <yju1028@naver.com>
2024-10-29 13:18:20 +0900

    충돌 해결

--- front/photorize/src/components/Common/Header.tsx
+++ front/photorize/src/components/Common/Header.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
-const Header = () => {
-  return <div>Header</div>;
+interface HeaderProps {
+  title: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
+  const handleNotificationClick = () => {
+    navigate("/notifications");
+  };
+
+  return (
+    <header className="flex items-center justify-between p-4 bg-[#F9F9F9]  shadow">
+      <div className="flex items-center">
+        {location.pathname === "/home" ? (
+          <img src="/assets/Logo1.png" alt="Photorize Logo" className="h-4" />
+        ) : (
+          <img
+            src="/assets/back-icon.png"
+            alt="Back Icon"
+            onClick={handleBackClick}
+            className="h-6"
+          />
+        )}
+      </div>
+      <h1 className="absolute left-1/2 transform -translate-x-1/2 text-lg font-semibold">
+        {title}
+      </h1>
+      {location.pathname !== "/notifications" && (
+        <img
+          src="/assets/notification-icon.png"
+          alt="Notification Icon"
+          onClick={handleNotificationClick}
+          className="h-6"
+        />
+      )}
+    </header>
+  );
 };
 
 export default Header;
--- front/photorize/src/pages/Home.tsx
+++ front/photorize/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import Header from "../components/Common/Header";
 const Home = () => {
   return (
     <div>
-      <Header />
+      <Header title="" />
       Home
     </div>
   );
