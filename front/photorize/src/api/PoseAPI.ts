cdd14527e56b3e3ade48c174cc99eb2f6ad8c215
chanhyun22 <bch0707@dongguk.edu>
2024-11-13 10:30:12 +0900

    Feat: 테스트

--- front/photorize/src/api/PoseAPI.ts
+++ front/photorize/src/api/PoseAPI.ts
@@ -3,6 +3,7 @@ import axios from "./axiosConfig";
 export const getAllPoses = async () => {
   try {
     const response = await axios.get("/poses");
+    console.log(response);
     return response.data;
   } catch (error) {
     console.error("포즈 전체조회 중 오류 발생:", error);
