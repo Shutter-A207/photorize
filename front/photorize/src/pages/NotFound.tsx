94f18a3e27ea73b1b08ee2e23d2c3c212bdf10b2
5ooyeon <melthetensai@gmail.com>
2024-11-14 10:07:01 +0900

    Fix: 404에러페이지 변경

--- front/photorize/src/pages/NotFound.tsx
+++ front/photorize/src/pages/NotFound.tsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from './NotFound.module.css';
 
 const NotFound: React.FC = () => {
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <img src="/assets/pineapple-icon.png" alt="로고" className={`w-[65%] mb-24 ${styles.rotate}`}  />
-      <div className="w-[80%] max-w-md mx-auto">
+  const [fallingPineapples, setFallingPineapples] = useState<number[]>([]);
 
-        <div
-          className="text-center text-[24px] text-[#686E74] mb-8 underline"
-        >
+  useEffect(() => {
+    const generatePineapples = () => {
+      const pineapples = Array.from({ length: 30 }, (_, i) => i);
+      setFallingPineapples(pineapples);
+    };
+
+    generatePineapples();
+  }, []);
+
+  const getRandomImage = () => {
+    const random = Math.random();
+    return random <= 0.03 ? "/assets/hidden-hangyo.png" : "/assets/pineapple-icon.png";
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center relative overflow-hidden">
+      {fallingPineapples.map((_, index) => (
+        <img
+          key={index}
+          src={getRandomImage()}
+          alt="Falling Pineapple"
+          className={styles.fallingPineapple}
+          style={{
+            '--random-x': Math.random(),
+            '--random-duration': `${Math.random() * 5 + 5}s`,
+          } as React.CSSProperties}
+        />
+      ))}
+      <img src="/assets/pineapple-icon.png" alt="로고" className={`w-[65%] mb-24 ${styles.rotate}`} />
+      <div className="w-[80%] max-w-md mx-auto">
+        <div className="text-center text-[24px] font-bold mb-8">
           잘못된 경로입니다.
         </div>
         <button
-          className="w-full h-[50px] font-bold text-[18px] p-3 mb-4 bg-[#FF93A5] text-white rounded-2xl"
+          className="w-full h-[50px] text-[18px] p-3 mb-4 bg-[#FF93A5] text-[#FFFFFF] rounded-2xl"
         >
           메인으로 돌아가기
         </button>
